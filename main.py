e2df00359a1775a785b6ed42278dfda566eb984a
Trung Ha <hachitrung024@gmail.com>
2024-12-09 19:24:29 +0700

    update result

--- main.py
+++ main.py
@@ -9,24 +9,32 @@ env = gym.make(
     "gym_cutting_stock/CuttingStock-v0",
     render_mode="human",  # Comment this line to disable rendering
 )
-NUM_EPISODES = 5
+NUM_EPISODES = 100
 
 if __name__ == "__main__":
-    # Reset the environment
-    observation, info = env.reset(seed=42)
+    # total_trim_loss = 0.0
+    # file_path = "student_submissions\s2213467\policy_id1_100eps.txt"
+    # with open(file_path, "w") as file:
+    #     pass
+    # # Reset the environment
+    # observation, info = env.reset(seed=42)
+    # # Test Policy 1
+    # policy = Policy2213467(policy_id=1)
+    # ep = 0
+    # while ep < NUM_EPISODES:
+    #     action = policy.get_action(observation, info)
+    #     observation, reward, terminated, truncated, info = env.step(action)
 
-    # Test Policy 1
-    policy = Policy2213467(policy_id=1)
-    ep = 0
-    while ep < NUM_EPISODES:
-        action = policy.get_action(observation, info)
-        observation, reward, terminated, truncated, info = env.step(action)
-
-        if terminated or truncated:
-            print(info)
-            observation, info = env.reset(seed=ep)
-            ep += 1
-
+    #     if terminated or truncated:
+    #         print(info)
+    #         with open(file_path, "a") as file:
+    #             file.write(f"{info}\n")
+    #         total_trim_loss+= info["trim_loss"]
+    #         observation, info = env.reset(seed=ep)
+    #         ep += 1
+    # print(f"Average trim_loss: {total_trim_loss/NUM_EPISODES} in {NUM_EPISODES} eps")
+    # with open(file_path, "a") as file:
+    #     file.write(f"Average trim_loss: {total_trim_loss/NUM_EPISODES} in {NUM_EPISODES} eps")
     # Reset the environment
     observation, info = env.reset(seed=42)
 
