2f19e1100fba3d6134ef24d4cee9238babd4aefd
Nguyễn Duy Thành <166360116+Tearsofroses@users.noreply.github.com>
2024-12-13 12:16:34 +0700

    Update policy2353101.py

--- student_submissions/s2353101/policy2353101.py
+++ student_submissions/s2353101/policy2353101.py
@@ -1,7 +1,7 @@
 from policy import Policy
 
 
-class Policy2210xxx(Policy):
+class Policy2353101(Policy):
     def __init__(self, policy_id=1):
         assert policy_id in [1, 2], "Policy ID must be 1 or 2"
 
