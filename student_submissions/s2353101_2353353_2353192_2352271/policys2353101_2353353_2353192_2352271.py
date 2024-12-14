19be99996a7f2bf1d53a378cdd26560f06bd7e40
Nguyễn Duy Thành <166360116+Tearsofroses@users.noreply.github.com>
2024-12-14 20:28:27 +0700

    Update folder and file name

--- student_submissions/s2353101_2353353_2353192_2352271/policys2353101_2353353_2353192_2352271.py
+++ student_submissions/s2353101_2353353_2353192_2352271/policys2353101_2353353_2353192_2352271.py
@@ -1,7 +1,7 @@
 from policy import Policy
 
 
-class Policy2353101(Policy):
+class Policys2353101_2353353_2353192_2352271(Policy):
     def __init__(self, policy_id=1):
         assert policy_id in [1, 2], "Policy ID must be 1 or 2"
 
