452073c60ba59df9b70efc2e43125f8989b7bd7d
Phúc Nhân <143471539+nhan2892005@users.noreply.github.com>
2024-12-15 22:33:51 +0700

    Update: Policy idx only in set {1, 2}

--- student_submissions/s_2312438_2312410_2311982/policy2312438_2312410_2311982.py
+++ student_submissions/s_2312438_2312410_2311982/policy2312438_2312410_2311982.py
@@ -3,7 +3,7 @@ from .src.best_fit import BestFit
 from .src.generate_column import GenerateColumn
 class Policy2312438_2312410_2311982(Policy):
     def __init__(self, policy_id=1):
-        assert policy_id in range(1,4), "Policy ID must be in [1, 4]"
+        assert policy_id in [1, 2], "Policy ID must be 1 or 2"
 
         if policy_id == 1:
             self.policy = BestFit()
